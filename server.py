a04576aa6806c7db5a42f16ad7136513b0ac06d2
SandraSk06 <sandraskaria@gmail.com>
2024-10-17 19:06:21 +0530

    Updated sequence

--- server.py
+++ server.py
@@ -12,6 +12,7 @@ from waitress import serve
 app = Flask(__name__)
 
 received_messages = []
+client_instance = None 
 
 # MQTT connection details
 broker = 'broker.emqx.io'
@@ -29,7 +30,7 @@ def on_message(client, userdata, msg):
         print("Failed to decode JSON ", msg.payload.decode())
 
 
-def on_connect(client, userdata, flags, rc):
+def on_connect(client, userdata, flags, rc, properties):
     if rc == 0:
         print("Connected to MQTT Broker")
         client.subscribe(topic)
@@ -37,138 +38,37 @@ def on_connect(client, userdata, flags, rc):
         print("Failed to connect, return code: ", rc)
 
 
-@app.route('/shipping/mqtt-subscribe')
-def connect_with_mqtt():
-    client = mqtt_client.Client(client_id=client_id, callback_api_version=mqtt_client.CallbackAPIVersion.VERSION2)
-    client.on_connect = on_connect
-    client.on_message = on_message
-    client.connect(broker, port)
+def connect_to_mqtt():
+    global client_instance
 
-    client.loop_start()
+    client_instance = mqtt_client.Client(client_id=client_id, callback_api_version=mqtt_client.CallbackAPIVersion.VERSION2)
+    client_instance.on_connect = on_connect
+    client_instance.on_message = on_message
+    client_instance.connect(broker, port)
+
+    client_instance.loop_start()
 
     try:
         print("No exception")
     except KeyboardInterrupt:
         print("Disconnect from Broker")
-        client.loop_stop()
-        client.disconnect()
+        client_instance.loop_stop()
+        client_instance.disconnect()
 
-    # mqtt_thread = threading.Thread(target=client.loop_forever)
-    # mqtt_thread.start()
-    #
-    # # Subscribe
-    # subscribe(client, topic)
+
+@app.route('/shipping/mqtt-subscribe')
+def connect_with_mqtt():
+    if client_instance is None:
+        connect_to_mqtt()
 
     return render_template('index.html')
 
 
 @app.route('/shipping/messages')
 def get_messages():
-    # publish()
     return jsonify(received_messages)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-def connect_mqtt() -> mqtt_client:
-    client = mqtt_client.Client(client_id=client_id, callback_api_version=mqtt_client.CallbackAPIVersion.VERSION2)
-    client.on_connect = on_connect
-    client.on_message = on_message
-    client.connect(broker, port)
-
-    client.loop_start()
-
-    try:
-        while True:
-            pass
-    except KeyboardInterrupt:
-        print("Disconnect from Broker")
-        client.loop_stop()
-        client.disconnect()
-
-
-
-    # For paho-mqtt 2.0.0, you need to add the properties parameter.
-    # def on_connect(client, userdata, flags, rc, properties):
-    #     if rc == 0:
-    #         print("Connected to MQTT Broker!")
-    #     else:
-    #         print("Failed to connect, return code %d\n", rc)
-    #
-    # # Set Connecting Client ID
-    # # client = mqtt_client.Client(client_id)
-    #
-    # # For paho-mqtt 2.0.0, you need to set callback_api_version.
-    # client = mqtt_client.Client(client_id=client_id, callback_api_version=mqtt_client.CallbackAPIVersion.VERSION2)
-    #
-    # client.on_connect = on_connect
-    # client.connect(broker, port)
-    #
-    # return client
-
-
-# def subscribe(client: mqtt_client, topic):
-#     def on_message(client, userdata, msg):
-#         try:
-#             message = json.loads(msg.payload.decode())
-#             print(f"Received `{message}` from `{msg.topic}` topic")
-#             received_messages.append(message)
-#         except json.JSONDecodeError:
-#             print("Failed to decode JSON")
-#
-#
-#     client.subscribe(topic)
-#     client.on_message = on_message
-#     client.loop_start()
-
-def publish():
-    broker = 'broker.emqx.io'
-    port = 1883
-    topic = "test"
-    client_id = f'python-mqtt-{random.randint(0, 1000)}'
-
-    client = mqtt_client.Client(client_id=client_id, callback_api_version=mqtt_client.CallbackAPIVersion.VERSION2)
-    client.connect(broker, port)
-    client.loop_start()
-
-    while True:
-        message = {'id': 1, 'msg': 'Hello from Python!!'}
-        client.publish(topic, json.dumps(message))
-        print(f'Published: {message}')
-        time.sleep(5)
-
-def load_shipping_data():
-    csv_file_path = r'ShippingLogs.csv'
-    json_file_path = r'ShippingJson.json'
-    convert_to_json(csv_file_path, json_file_path)
-
-    return render_template('index.html')
-
-
-@app.route('/shipping/get-data-by-date', methods=['POST'])
-def get_shipping_data_by_date():
-    # Get parameters from the request body (JSON)
-    data = request.get_json()
-    start_date = data.get('start_date')
-    end_date = data.get('end_date')
-
-    filtered_data = get_data_sort_by_date(start_date, end_date)
-    return jsonify(filtered_data)
 
 
 if __name__ == "__main__":
     serve(app, host="0.0.0.0", port=8000)
     app.run(debug=True)
-
-
-
--- static/scripts/onboard.js
+++ static/scripts/onboard.js
@@ -1,79 +1,3 @@
-async function showListFromDate() {
-
-    const start_date_value = document.querySelector('.js-input-start-date').value;
-    const end_date_value = document.querySelector('.js-input-end-date').value;
-
-    try {
-        // Send POST request with parameters
-        const response = await fetch('http://localhost:8000/shipping/get-data-by-date', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ start_date: start_date_value, end_date: end_date_value })
-        });
-
-        // Check if the response is successful
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-
-        // Parse the JSON response
-        const data = await response.json();
-
-        // Check if data exists
-        if (data.length === 0) {
-            console.log("No data found for the given date range.");
-            showMessage("No data found for the given date range.");
-            return;
-        }
-
-        // Display the result
-        const tableBody = document.querySelector('#js-show-result tbody');
-
-         // Clear the table body before adding new data
-        tableBody.innerHTML = '';
-
-        const initialSequenceElement = document.getElementById("sequence-diagram-container").innerHTML;
-
-        // Iterate through each item in the JSON data
-        data.forEach(item => {
-            const row = document.createElement('tr'); // Create a new row
-
-            // Selected fields
-            const selectedFields = ['OrderID', 'Carrier', 'ShippingOrigin', 'OrderDate', 'OnTimeDelivery'];
-
-            // Loop through the selected fields and append their values to the row
-            selectedFields.forEach(field => {
-                const cell = document.createElement('td');
-                cell.textContent = item[field];
-                row.appendChild(cell);
-            });
-
-            // Append the row to the table body
-            tableBody.appendChild(row);
-
-            row.addEventListener("click", ()=> {
-                const popupContent = document.getElementById("sequence-diagram-container");
-
-                popupContent.innerHTML = initialSequenceElement;
-                document.getElementById("overlay").style.display = "flex";
-
-                loadSequenceDiagram(popupContent, item);
-
-                // document.getElementById("overlay").innerHTML='<object type="text/html" data="templates/popup.html" ></object>';
-                // alert(`Order Id: ${item.OrderID}\n Shipping Origin: ${item.ShippingOrigin}`);
-            });
-        });
-
-
-    } catch (error) {
-        console.log('Error:', error);
-        showMessage("Failed to load data.");
-    }
-
-}
-
 function closePopup() {
     // Close popup functionality
     document.getElementById("overlay").style.display = "none";  // Hide the overlay
@@ -101,6 +25,7 @@ async function connectWithMqtt() {
 
 // Receive messages from MQTT
 async function getMessages() {
+    const messageById = {};
 
     const response = await fetch('http://localhost:8000/shipping/messages');
     // Check if the response is successful
@@ -115,6 +40,17 @@ async function getMessages() {
         console.log("No data found for the given date range.");
         showMessage("No data found for the given date range.");
         return;
+    } else {
+        data.forEach(message => {
+            const messageId = message.id;
+            if (!messageById[messageId]) {
+                messageById[messageId] = {
+                    id: messageId,
+                    msg: []
+                };
+            }
+            messageById[messageId].msg.push(message.msg);
+        });
     }
 
     // Display the result
@@ -122,6 +58,8 @@ async function getMessages() {
 
     // Clear the table body before adding new data
     tableBody.innerHTML = '';
+
+    const initialSequenceElement = document.getElementById("sequence-diagram-container").innerHTML;
 
      // Iterate through each item in the JSON data
      data.forEach(item => {
@@ -139,18 +77,34 @@ async function getMessages() {
 
         // Append the row to the table body
         tableBody.appendChild(row);
+
+        row.addEventListener("click", ()=> {
+            const popupContent = document.getElementById("sequence-diagram-container");
+
+            popupContent.innerHTML = initialSequenceElement;
+            document.getElementById("overlay").style.display = "flex";
+
+            if (messageById.hasOwnProperty(item.id) &&  messageById[item.id].msg.length > 0) {        
+                loadSequenceDiagram(popupContent, item.id, messageById[item.id].msg);
+            }
+
+        });
      });
 }
 
 
-function loadSequenceDiagram(popupContent, item) {
-    const diagramDesc = `${item.ShippingOrigin}->${item.OnTimeDelivery}: ${item.ActualShippingDays} days
-    `;
+function loadSequenceDiagram(popupContent, itemId, messages) {
+    let diagramDesc = '';
+
+    for (const msg of messages) {
+        diagramDesc += `${itemId}->Messages: ${msg} 
+        `;
+    }
     document.getElementById("loading").style.display = 'block';
 
     const myDiagram = Diagram.parse(diagramDesc);
     myDiagram.drawSVG(popupContent, { theme: 'simple' });
-
+    
     setTimeout(()=> {
 
     document.getElementById("loading").style.display = 'none';
